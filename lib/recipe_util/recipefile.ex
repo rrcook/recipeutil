7f575074063f60afa223cadb708766e203c6e604
Richard Cook <blubparadox@gmail.com>
2023-05-27 21:32:05 -0400

    When a destination file is given, generate bytes from the ReciptType struct and write to the file.

--- lib/recipe_util/recipefile.ex
+++ lib/recipe_util/recipefile.ex
@@ -6,8 +6,17 @@ defmodule RecipeUtil.File do
     with {:ok, file} <- File.open(source),
     data <- IO.binread(file, :eof),
      :ok <- File.close(file) do
-      recipe_type = RecipeType.parse(data)
-      IO.inspect(recipe_type)
+      {:ok, recipe, _rest} = RecipeType.parse(data)
+      # IO.inspect(recipe_type)
+      recipe_bytes = RecipeType.generate(recipe)
+      if (dest) do
+        with {:ok, file} <- File.open(dest, [:write, :binary]) do
+          IO.binwrite(file, recipe_bytes)
+          File.close(file)
+        end
+      else
+        IO.inspect(recipe)
+      end
     end
   end
 end
